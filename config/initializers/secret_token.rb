bcad4ad4655c51f49b85e62d7463c3b8ef30e73f
Andrew Slotin <andrew.slotin@gmail.com>
2013-03-08 21:34:16 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookshelf::Application.config.secret_key_base = 'b8c86092eca56f8d0e8f5563a2889e896eb406be4243d2d34fa6cd37da890084926f52ec4c0280f33373248feba8dd87ffa7082a8e3320947cb0524a648ea24f'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Bookshelf::Application.routes.draw do
+  root to: 'application#index'
+end
